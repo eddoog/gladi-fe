e64393825e30a0477097902a893e7fdd87356a5c
eddoog <codewithalfredo@gmail.com>
2024-05-05 19:50:27 +0700

    chore: try deploy

--- src/modules/LandingPage.tsx
+++ src/modules/LandingPage.tsx
@@ -1,5 +1,5 @@
 export function LandingPage() {
   return (
-    <div className="flex text-5xl font-bold flex-1 items-center">Gladi</div>
+    <div className="flex text-5xl font-bold flex-1 items-center">Gladi.</div>
   );
 }
