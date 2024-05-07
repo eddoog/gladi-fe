c27291ae54112dc6d56b13ccd35641e85f0e9ded
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2024-05-07 15:22:51 +0700

    Add search bar and create recording button on recording list page.

--- src/modules/RecordingListPage.tsx
+++ src/modules/RecordingListPage.tsx
@@ -13,7 +13,21 @@ export function RecordingListPage() {
                 <h2 className="text-3xl md:text-5xl text-center w-full font-bold">
                     Your Recordings
                 </h2>
-                <div className="flex flex-wrap gap-3 w-full py-10">
+                <div className="flex flex-wrap gap-3 w-full py-5">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="w-3/4"
+                    value=""
+                />
+                <button
+                    className="rounded-2xl flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 w-1/5"
+                >
+                    Create Recording
+                </button>
+                </div>
+                
+                <div className="flex flex-wrap gap-3 w-full py-5">
                     <div className="w-full">
                         <a href="" className="block">
                             <div className="w-full bg-gray-200 p-4 rounded-lg shadow-md mb-4">
@@ -37,7 +51,7 @@ export function RecordingListPage() {
                 </div>
                 <button 
                     onClick={handleBack} 
-                    className="rounded-2xl flex flex-row items-center gap-2 justify-center hover:text-blue-300"
+                    className="rounded-2xl flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 px-4 py-2"
                 >
                     Back
                 </button>
