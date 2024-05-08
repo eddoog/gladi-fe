1f388a85e667569d0b5576329a9ed82356df2e14
eddoog <codewithalfredo@gmail.com>
2024-05-08 15:34:48 +0700

    feat: design some more

--- src/modules/CapturePage.tsx
+++ src/modules/CapturePage.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { useRecordWebcam } from 'react-record-webcam';
-import { CameraButton } from '../components/common/CameraButton';
-import { Select } from '../components/common/Select';
+import React from "react";
+import { useRecordWebcam } from "react-record-webcam";
+import { CameraButton } from "../components/common/CameraButton";
+import { Select } from "../components/common/Select";
+import { useNavigate } from "react-router-dom";
 
 export function CapturePage() {
+  const navigate = useNavigate();
+
   const {
     activeRecordings,
     cancelRecording,
@@ -24,16 +27,16 @@ export function CapturePage() {
     stopRecording,
   } = useRecordWebcam();
 
-  const [videoDeviceId, setVideoDeviceId] = React.useState<string>('');
-  const [audioDeviceId, setAudioDeviceId] = React.useState<string>('');
+  const [videoDeviceId, setVideoDeviceId] = React.useState<string>("");
+  const [audioDeviceId, setAudioDeviceId] = React.useState<string>("");
 
   const handleSelect = async (event: any) => {
     const { deviceid: deviceId } =
       event.target.options[event.target.selectedIndex].dataset;
-    if (devicesById?.[deviceId].type === 'videoinput') {
+    if (devicesById?.[deviceId].type === "videoinput") {
       setVideoDeviceId(deviceId);
     }
-    if (devicesById?.[deviceId].type === 'audioinput') {
+    if (devicesById?.[deviceId].type === "audioinput") {
       setAudioDeviceId(deviceId);
     }
   };
@@ -57,109 +60,132 @@ export function CapturePage() {
     if (recording) await openCamera(recording.id);
   };
 
-  return (
-    <div className='container'>
-        <div className="mx-auto p-4" style={{maxWidth: "1000px", width: "70%"}}>
-            <div className="space-y-2 my-4">
-                <div className="flex">
-                <h4>Select video input</h4>
-                <Select
-                    items={devicesByType?.video || []}
-                    dataset="deviceid"
-                    onChange={handleSelect}
-                />
-                </div>
-                <div className="flex">
-                <h4>Select audio input</h4>
-                <Select
-                    items={devicesByType?.audio || []}
-                    dataset="deviceid"
-                    onChange={handleSelect}
-                />
-                </div>
-            </div>
-            <div className="space-x-2">
-                <CameraButton onClick={quickDemo}>Tes Perekaman</CameraButton>
-                <CameraButton onClick={start}>Buka Kamera</CameraButton>
-                <CameraButton onClick={() => clearAllRecordings()}>Hapus Rekaman</CameraButton>
-                <CameraButton onClick={() => clearError()}>Hapus Log Eror</CameraButton>
-            </div>
-            <div className="my-2">
-                <p>{errorMessage ? `Error: ${errorMessage}` : ''}</p>
-            </div>
-            <div className="grid grid-cols-custom gap-4 my-4">
-                {activeRecordings?.map((recording) => (
-                <div className="bg-white rounded-lg px-4 py-4" key={recording.id}>
-                    <div className="text-black grid grid-cols-1">
-                    <p>Live</p>
-                    <small>Status: {recording.status}</small>
-                    <small>Video: {recording.videoLabel}</small>
-                    <small>Audio: {recording.audioLabel}</small>
-                    </div>
-                    <video ref={recording.webcamRef} loop autoPlay playsInline />
-                    <div className="space-x-1 space-y-1 my-2">
-                    <CameraButton
-                        inverted
-                        disabled={
-                        recording.status === 'RECORDING' ||
-                        recording.status === 'PAUSED'
-                        }
-                        onClick={() => startRecording(recording.id)}
-                    >
-                        Rekam
-                    </CameraButton>
-                    <CameraButton
-                        inverted
-                        disabled={
-                        recording.status !== 'RECORDING' &&
-                        recording.status !== 'PAUSED'
-                        }
-                        toggled={recording.status === 'PAUSED'}
-                        onClick={() =>
-                        recording.status === 'PAUSED'
-                            ? resumeRecording(recording.id)
-                            : pauseRecording(recording.id)
-                        }
-                    >
-                        {recording.status === 'PAUSED' ? 'Lanjut' : 'Jeda'}
-                    </CameraButton>
-                    <CameraButton
-                        inverted
-                        toggled={recording.isMuted}
-                        onClick={() => muteRecording(recording.id)}
-                    >
-                        Mic
-                    </CameraButton>
-                    <CameraButton inverted onClick={() => stopRecording(recording.id)}>
-                        Berhenti
-                    </CameraButton>
-                    <CameraButton inverted onClick={() => cancelRecording(recording.id)}>
-                        Batal
-                    </CameraButton>
-                    </div>
+  const handleBack = () => {
+    navigate(-1);
+  };
 
-                    <div
-                    className={`${
-                        recording.previewRef.current?.src.startsWith('blob:')
-                        ? 'visible'
-                        : 'hidden'
-                    }`}
-                    >
-                    <p>Pratinjau</p>
-                    <video ref={recording.previewRef} autoPlay loop playsInline />
-                    <div className="space-x-2 my-2">
-                        <CameraButton inverted onClick={() => download(recording.id)}>
-                        Unduh
-                        </CameraButton>
-                        <CameraButton inverted onClick={() => clearPreview(recording.id)}>
-                        Bersihkan Pratinjau
-                        </CameraButton>
-                    </div>
-                    </div>
-                </div>
-                ))}
-            </div>
+  return (
+    <div className="container">
+      <div className="mx-auto p-4" style={{ maxWidth: "1000px", width: "70%" }}>
+        <button
+          onClick={handleBack}
+          className="rounded-lg flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 dark:bg-gray-500 hover:dark:bg-gray-600 duration-200 transition-all ease-in-out px-4 py-2"
+        >
+          Back
+        </button>
+        <div className="space-y-2 my-4">
+          <div className="flex">
+            <h4>Select video input</h4>
+            <Select
+              items={devicesByType?.video || []}
+              dataset="deviceid"
+              onChange={handleSelect}
+            />
+          </div>
+          <div className="flex">
+            <h4>Select audio input</h4>
+            <Select
+              items={devicesByType?.audio || []}
+              dataset="deviceid"
+              onChange={handleSelect}
+            />
+          </div>
         </div>
+        <div className="space-x-2">
+          <CameraButton onClick={quickDemo}>Tes Perekaman</CameraButton>
+          <CameraButton onClick={start}>Buka Kamera</CameraButton>
+          <CameraButton onClick={() => clearAllRecordings()}>
+            Hapus Rekaman
+          </CameraButton>
+          <CameraButton onClick={() => clearError()}>
+            Hapus Log Eror
+          </CameraButton>
+        </div>
+        <div className="my-2">
+          <p>{errorMessage ? `Error: ${errorMessage}` : ""}</p>
+        </div>
+        <div className="grid grid-cols-custom gap-4 my-4">
+          {activeRecordings?.map((recording) => (
+            <div className="bg-white rounded-lg px-4 py-4" key={recording.id}>
+              <div className="text-black grid grid-cols-1">
+                <p>Live</p>
+                <small>Status: {recording.status}</small>
+                <small>Video: {recording.videoLabel}</small>
+                <small>Audio: {recording.audioLabel}</small>
+              </div>
+              <video ref={recording.webcamRef} loop autoPlay playsInline />
+              <div className="space-x-1 space-y-1 my-2">
+                <CameraButton
+                  inverted
+                  disabled={
+                    recording.status === "RECORDING" ||
+                    recording.status === "PAUSED"
+                  }
+                  onClick={() => startRecording(recording.id)}
+                >
+                  Rekam
+                </CameraButton>
+                <CameraButton
+                  inverted
+                  disabled={
+                    recording.status !== "RECORDING" &&
+                    recording.status !== "PAUSED"
+                  }
+                  toggled={recording.status === "PAUSED"}
+                  onClick={() =>
+                    recording.status === "PAUSED"
+                      ? resumeRecording(recording.id)
+                      : pauseRecording(recording.id)
+                  }
+                >
+                  {recording.status === "PAUSED" ? "Lanjut" : "Jeda"}
+                </CameraButton>
+                <CameraButton
+                  inverted
+                  toggled={recording.isMuted}
+                  onClick={() => muteRecording(recording.id)}
+                >
+                  Mic
+                </CameraButton>
+                <CameraButton
+                  inverted
+                  onClick={() => stopRecording(recording.id)}
+                >
+                  Berhenti
+                </CameraButton>
+                <CameraButton
+                  inverted
+                  onClick={() => cancelRecording(recording.id)}
+                >
+                  Batal
+                </CameraButton>
+              </div>
+
+              <div
+                className={`${
+                  recording.previewRef.current?.src.startsWith("blob:")
+                    ? "visible"
+                    : "hidden"
+                }`}
+              >
+                <p>Pratinjau</p>
+                <video ref={recording.previewRef} autoPlay loop playsInline />
+                <div className="space-x-2 my-2">
+                  <CameraButton inverted onClick={() => download(recording.id)}>
+                    Unduh
+                  </CameraButton>
+                  <CameraButton
+                    inverted
+                    onClick={() => clearPreview(recording.id)}
+                  >
+                    Bersihkan Pratinjau
+                  </CameraButton>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/modules/RecordingListPage.tsx
+++ src/modules/RecordingListPage.tsx
@@ -1,62 +1,61 @@
 import { useNavigate } from "react-router-dom";
 
 export function RecordingListPage() {
-    const navigate = useNavigate();
-    
-    const handleBack = () => {
-        navigate("/");
-      };
-    
-    return (
-        <div> 
-            <div className="flex flex-wrap gap-3 w-full py-10">
-                <h2 className="text-3xl md:text-5xl text-center w-full font-bold">
-                    Your Recordings
-                </h2>
-                <div className="flex flex-wrap gap-3 w-full py-5">
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="w-3/4"
-                    value=""
-                />
-                <button
-                    className="rounded-2xl flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 w-1/5"
-                >
-                    Create Recording
-                </button>
-                </div>
-                
-                <div className="flex flex-wrap gap-3 w-full py-5">
-                    <div className="w-full">
-                        <a href="" className="block">
-                            <div className="w-full bg-gray-200 p-4 rounded-lg shadow-md mb-4">
-                                <p>Video 1</p>
-                                <p>Time: ...</p>
-                            </div>
-                        </a>
-                        <a href="" className="block">
-                            <div className="w-full bg-gray-200 p-4 rounded-lg shadow-md mb-4">
-                                <p>Video 2</p>
-                                <p>Time: ...</p>
-                            </div>
-                        </a>
-                        <a href="" className="block">
-                            <div className="w-full bg-gray-200 p-4 rounded-lg shadow-md mb-4">
-                                <p>Video 3</p>
-                                <p>Time: ...</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <button 
-                    onClick={handleBack} 
-                    className="rounded-2xl flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 px-4 py-2"
-                >
-                    Back
-                </button>
-            </div> 
-            
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <div className="flex flex-wrap gap-3 w-full py-10">
+        <h2 className="text-3xl md:text-5xl text-center w-full font-bold">
+          Your Recordings
+        </h2>
+        <div className="flex flex-wrap gap-3 w-full py-5">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-3/4 px-4 rounded-lg"
+            value=""
+          />
+          <button className="rounded-lg flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 dark:bg-gray-500 hover:dark:bg-gray-600 px-4 py-2 w-1/5 duration-200 transition-all ease-in-out">
+            <a href="/capture" className="text-white">
+              Create Recording
+            </a>
+          </button>
         </div>
-    )
+
+        <div className="flex flex-wrap gap-3 w-full py-5">
+          <div className="w-full">
+            <a href="/" className="block">
+              <div className="w-full bg-gray-200 p-4 rounded-lg shadow-md mb-4">
+                <p>Video 1</p>
+                <p>Time: ...</p>
+              </div>
+            </a>
+            <a href="/" className="block">
+              <div className="w-full bg-gray-200 p-4 rounded-lg shadow-md mb-4">
+                <p>Video 2</p>
+                <p>Time: ...</p>
+              </div>
+            </a>
+            <a href="/" className="block">
+              <div className="w-full bg-gray-200 p-4 rounded-lg shadow-md mb-4">
+                <p>Video 3</p>
+                <p>Time: ...</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <button
+          onClick={handleBack}
+          className="rounded-lg flex flex-row items-center gap-2 justify-center bg-blue-500 text-white hover:bg-blue-600 dark:bg-gray-500 hover:dark:bg-gray-600 duration-200 transition-all ease-in-out px-4 py-2"
+        >
+          Back
+        </button>
+      </div>
+    </div>
+  );
 }
